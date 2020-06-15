a386756ffb916a7778110df70bd8820c7799383a
wangshiphys <wangshiphys@gmail.com>
2020-06-15 15:28:12 +0800

    Minor changes to `ShowModel.py` in `Phase12` sub-package.

--- Phase12/ShowModel.py
+++ Phase12/ShowModel.py
@@ -12,19 +12,19 @@ intra_bonds_2nd, inter_bonds_2nd = cluster.bonds(nth=2)
 fig, ax = plt.subplots()
 for point in cluster_points:
     cluster_index = cluster.getIndex(point, fold=True)
-    if cluster_index in tuple(range(12)):
-        color = "black"
-        marker_size = 12
-    else:
+    if cluster_index == 12:
         color = "tab:red"
-        marker_size = 15
+        marker_size = 20
+    else:
+        color = "black"
+        marker_size = 16
     ax.plot(
         point[0], point[1], marker="o",
         ms=marker_size, color=color, zorder=1
     )
     ax.text(
         point[0], point[1] - 0.2, str(cluster_index),
-        ha="center", va="top", fontsize="x-large", zorder=2
+        ha="center", va="top", fontsize="xx-large", zorder=2
     )
 for bond in intra_bonds_1st:
     p0, p1 = bond.endpoints
@@ -36,12 +36,12 @@ for bond in intra_bonds_2nd:
     p0, p1 = bond.endpoints
     ax.plot(
         [p0[0], p1[0]], [p0[1], p1[1]],
-        lw=3, color="tab:blue", zorder=0
+        ls="solid", lw=3, color="tab:blue", zorder=0
     )
 
 ax.set_axis_off()
 ax.set_aspect("equal")
-fig.set_size_inches(4.7, 4.2)
+plt.get_current_fig_manager().window.showMaximized()
+plt.tight_layout()
 plt.show()
-fig.savefig("Model.png", dpi=300, transparent=True)
 plt.close("all")
