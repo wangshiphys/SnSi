966b834bb326e56c89296c3b4adec4d55a3fc4dd
wangshiphys <wangshiphys@gmail.com>
2020-06-15 15:22:01 +0800

    Minor changes to `database.py` in `Phase12` sub-package.

--- Phase12/database.py
+++ Phase12/database.py
@@ -19,6 +19,8 @@ POINTS = np.array(
         [-2.0, -SQRT3], [0.0, -SQRT3], [2.0, -SQRT3],
     ], dtype=np.float64
 )
+POINTS.setflags(write=False)
+VECTORS.setflags(write=False)
 
 
 if __name__ == "__main__":
@@ -26,9 +28,12 @@ if __name__ == "__main__":
     for i in [-1, 0, 1]:
         for j in [-1, 0, 1]:
             points = np.dot([i, j], VECTORS) + POINTS
-            ax.plot(points[:, 0], points[:, 1], ls="", marker="o", ms=6)
+            ax.plot(points[:, 0], points[:, 1], ls="", marker="o", ms=10)
     ax.set_axis_off()
     ax.set_aspect("equal")
+
+    plt.get_current_fig_manager().window.showMaximized()
+    plt.tight_layout()
     plt.show()
     plt.close("all")
 
