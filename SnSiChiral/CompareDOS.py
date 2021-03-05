3c19283b9ba0ac517a05e35a407b9530cab07b81
wangshiphys <wangshiphys@gmail.com>
2021-03-05 14:43:54 +0800

    Minor changes to `CompareDOS.py` in `SnSiChiral/` sub-package.

--- SnSiChiral/CompareDOS.py
+++ SnSiChiral/CompareDOS.py
@@ -4,40 +4,50 @@ import numpy as np
 from utilities import Mu
 
 site_num = 12
-data_name_temp = "data/DOS_{model}_t0={t0:.3f}_t1={t1:.3f}_U={U:.3f}.npz"
+data_name_temp = "data/dos/DOS_{model}_t0={t0:.3f}_t1={t1:.3f}_U={U:.3f}.npz"
 
 ids = [
-    {"model": "Model1", "t0": -1.00, "t1": -1.00, "U": 0.00},
-    {"model": "Model1", "t0": -1.00, "t1": -1.00, "U": 1.00},
-    {"model": "Model1", "t0": -1.00, "t1": -1.00, "U": 2.00},
-    {"model": "Model1", "t0": -1.00, "t1": -1.00, "U": 3.00},
-    {"model": "Model1", "t0": -1.00, "t1": -1.00, "U": 4.00},
-    {"model": "Model1", "t0": -1.00, "t1": -1.00, "U": 5.00},
-    {"model": "Model1", "t0": -1.00, "t1": -1.00, "U": 6.00},
-    {"model": "Model1", "t0": -1.00, "t1": -1.00, "U": 7.00},
-    {"model": "Model1", "t0": -1.00, "t1": -1.00, "U": 8.00},
-    {"model": "Model1", "t0": -1.00, "t1": -1.00, "U": 9.00},
+    {"model": "Model1", "t0": -1.00, "t1": -0.5, "U": 0.00},
+    {"model": "Model1", "t0": -1.00, "t1": -0.5, "U": 0.50},
+    {"model": "Model1", "t0": -1.00, "t1": -0.5, "U": 1.00},
+    {"model": "Model1", "t0": -1.00, "t1": -0.5, "U": 1.50},
+    {"model": "Model1", "t0": -1.00, "t1": -0.5, "U": 2.00},
+    {"model": "Model1", "t0": -1.00, "t1": -0.5, "U": 2.50},
+    {"model": "Model1", "t0": -1.00, "t1": -0.5, "U": 3.00},
+    {"model": "Model1", "t0": -1.00, "t1": -0.5, "U": 3.50},
+    {"model": "Model1", "t0": -1.00, "t1": -0.5, "U": 4.00},
+    {"model": "Model1", "t0": -1.00, "t1": -0.5, "U": 4.50},
+    {"model": "Model1", "t0": -1.00, "t1": -0.5, "U": 5.00},
+    {"model": "Model1", "t0": -1.00, "t1": -0.5, "U": 5.50},
+    {"model": "Model1", "t0": -1.00, "t1": -0.5, "U": 6.00},
+    {"model": "Model1", "t0": -1.00, "t1": -0.5, "U": 6.50},
+    {"model": "Model1", "t0": -1.00, "t1": -0.5, "U": 7.00},
+    {"model": "Model1", "t0": -1.00, "t1": -0.5, "U": 7.50},
+    {"model": "Model1", "t0": -1.00, "t1": -0.5, "U": 8.00},
+    {"model": "Model1", "t0": -1.00, "t1": -0.5, "U": 8.50},
+    {"model": "Model1", "t0": -1.00, "t1": -0.5, "U": 9.00},
+    {"model": "Model1", "t0": -1.00, "t1": -0.5, "U": 9.50},
 ]
 
-# ids = [
-#     {"model": "Model1", "t0": -1.00, "t1": 0.00, "U": 6.00},
-#     {"model": "Model1", "t0": -1.00, "t1": -0.10, "U": 6.00},
-#     {"model": "Model1", "t0": -1.00, "t1": -0.20, "U": 6.00},
-#     {"model": "Model1", "t0": -1.00, "t1": -0.30, "U": 6.00},
-#     {"model": "Model1", "t0": -1.00, "t1": -0.40, "U": 6.00},
-#     {"model": "Model1", "t0": -1.00, "t1": -0.50, "U": 6.00},
-#     {"model": "Model1", "t0": -1.00, "t1": -0.60, "U": 6.00},
-#     {"model": "Model1", "t0": -1.00, "t1": -0.70, "U": 6.00},
-#     {"model": "Model1", "t0": -1.00, "t1": -0.80, "U": 6.00},
-#     {"model": "Model1", "t0": -1.00, "t1": -0.90, "U": 6.00},
-#     {"model": "Model1", "t0": -1.00, "t1": -1.00, "U": 6.00},
-# ]
+ids = [
+    {"model": "Model1", "t0": -1.00, "t1":  0.00, "U": 5.00},
+    {"model": "Model1", "t0": -1.00, "t1": -0.10, "U": 5.00},
+    {"model": "Model1", "t0": -1.00, "t1": -0.20, "U": 5.00},
+    {"model": "Model1", "t0": -1.00, "t1": -0.30, "U": 5.00},
+    {"model": "Model1", "t0": -1.00, "t1": -0.40, "U": 5.00},
+    {"model": "Model1", "t0": -1.00, "t1": -0.50, "U": 5.00},
+    {"model": "Model1", "t0": -1.00, "t1": -0.60, "U": 5.00},
+    {"model": "Model1", "t0": -1.00, "t1": -0.70, "U": 5.00},
+    {"model": "Model1", "t0": -1.00, "t1": -0.80, "U": 5.00},
+    {"model": "Model1", "t0": -1.00, "t1": -0.90, "U": 5.00},
+    {"model": "Model1", "t0": -1.00, "t1": -1.00, "U": 5.00},
+]
 
 lines = []
 labels = []
 yticks = []
 baseline = 0.0
-interval = 0.5
+interval = 0.3
 fig, ax = plt.subplots()
 for id in ids:
     with np.load(data_name_temp.format(**id)) as ld:
@@ -56,9 +66,9 @@ for id in ids:
     baseline += interval
 ax.set_yticks(yticks)
 ax.grid(axis="both", ls="dashed", color="gray")
-ax.legend(lines[::-1], labels[::-1], loc="lower right", fontsize="xx-large")
+ax.legend(lines[::-1], labels[::-1], loc="lower left", fontsize=15)
+ax.set_title("t0={t0:.2f},U={U:.2f}".format(**ids[0]), fontsize=15)
 
 plt.get_current_fig_manager().window.showMaximized()
-plt.tight_layout()
 plt.show()
 plt.close("all")
