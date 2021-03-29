1526bb793e0cb6d5d16c7cb1c0af1e6581a51a1b
wangshiphys <wangshiphys@gmail.com>
2021-03-29 10:30:55 +0800

    A new design of `ShowModel.py` in `Phase320/` sub-package.

--- Phase320/ShowModel.py
+++ Phase320/ShowModel.py
@@ -34,9 +34,6 @@ def Rotation2D(theta, *, deg=False):
     return np.array([[cos_theta, -sin_theta], [sin_theta, cos_theta]])
 
 
-model = "Model1"
-
-# rotation_angle = 0
 rotation_angle = -np.arctan2(ALL_POINTS[6, 1], ALL_POINTS[6, 0])
 rotation_matrix = Rotation2D(rotation_angle).T
 all_points = np.dot(ALL_POINTS, rotation_matrix)
@@ -45,39 +42,49 @@ cell = Lattice(all_points[0:20], translation_vectors)
 points_collection = np.concatenate(
     [
         np.dot([i, j], translation_vectors) + all_points[0:20]
-        for i, j in [[0, 0], [-1, 0], [1, 0], [0, -1], [0, 1]]
+        for i, j in [[0, 0], [0, -1], [1, -1], [1, 0]]
     ]
 )
-
-intra_hopping_indices = [
-    [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7], [0, 8], [0, 9],
-    [1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7], [7, 8], [8, 9], [9, 1],
+intra_hopping_indices0 = [
+    [0, 1], [0, 2], [0, 3], [0, 4],
+    [0, 5], [0, 6], [0, 7], [0, 8], [0, 9],
     [10, 11], [10, 12], [10, 13], [10, 14],
     [10, 15], [10, 16], [10, 17], [10, 18], [10, 19],
+    [30, 31], [30, 32], [30, 33], [30, 34],
+    [30, 35], [30, 36], [30, 37], [30, 38], [30, 39],
+    [40, 41], [40, 42], [40, 43], [40, 44],
+    [40, 45], [40, 46], [40, 47], [40, 48], [40, 49],
+    [50, 51], [50, 52], [50, 53], [50, 54],
+    [50, 55], [50, 56], [50, 57], [50, 58], [50, 59],
+    [60, 61], [60, 62], [60, 63], [60, 64],
+    [60, 65], [60, 66], [60, 67], [60, 68], [60, 69],
+]
+intra_hopping_indices1 = [
+    [6, 11], [46, 51],
+    [1, 2], [2, 3], [3, 4], [4, 5],
+    [5, 6], [6, 7], [7, 8], [8, 9], [9, 1],
     [11, 12], [12, 13], [13, 14], [14, 15],
     [15, 16], [16, 17], [17, 18], [18, 19], [19, 11],
-    [6, 11],
+    [31, 32], [32, 33], [33, 34], [34, 35],
+    [35, 36], [36, 37], [37, 38], [38, 39], [39, 31],
+    [41, 42], [42, 43], [43, 44], [44, 45],
+    [45, 46], [46, 47], [47, 48], [48, 49], [49, 41],
+    [51, 52], [52, 53], [53, 54], [54, 55],
+    [55, 56], [56, 57], [57, 58], [58, 59], [59, 51],
+    [61, 62], [62, 63], [63, 64], [64, 65],
+    [65, 66], [66, 67], [67, 68], [68, 69], [69, 61],
 ]
-inter_hopping_indices = [[3, 37], [9, 74], [14, 89], [17, 43]]
-if model == "Model2":
-    intra_hopping_indices += [[6, 12], [6, 19], [5, 11], [7, 11]]
-    inter_hopping_indices += [
-        [3, 36], [3, 38], [2, 37], [4, 37],
-        [9, 73], [9, 75], [1, 74], [8, 74],
-        [14, 81], [14, 88], [13, 89], [15, 89],
-        [17, 42], [17, 44], [16, 43], [18, 43],
-    ]
-all_hopping_indices = intra_hopping_indices + inter_hopping_indices
+inter_hopping_indices = [[9, 34], [37, 43], [54, 69], [17, 63]]
 
-fig, ax = plt.subplots(num=model)
+fig, ax = plt.subplots()
 for index, point in enumerate(points_collection):
     cell_index = cell.getIndex(point, fold=True)
     if cell_index in (0, 10):
         color = "tab:red"
-        marker_size = 25
+        marker_size = 30
     else:
-        color = "tab:green"
-        marker_size = 20
+        color = "tab:blue"
+        marker_size = 25
 
     ax.plot(
         point[0], point[1],
@@ -85,26 +92,37 @@ for index, point in enumerate(points_collection):
     )
     ax.text(
         point[0], point[1],
-        str(index),
-        # str(cell_index),
-        ha="center", va="center", fontsize="x-large", zorder=2, clip_on=True,
+        # str(index),
+        str(cell_index),
+        ha="center", va="center", color="black",
+        fontsize=18, zorder=2, clip_on=True,
     )
 
-for ij in intra_hopping_indices:
+for ij in intra_hopping_indices0:
     bond = points_collection[ij]
-    ax.plot(
-        bond[:, 0], bond[:, 1],
-        # color="black",
+    line0, = ax.plot(
+        bond[:, 0], bond[:, 1], color="tab:red",
+        ls="solid", lw=4.0, zorder=0
+    )
+for ij in intra_hopping_indices1:
+    bond = points_collection[ij]
+    line1, = ax.plot(
+        bond[:, 0], bond[:, 1], color="tab:blue",
         ls="solid", lw=4.0, zorder=0
     )
 for ij in inter_hopping_indices:
     bond = points_collection[ij]
-    ax.plot(
-        bond[:, 0], bond[:, 1],
-        # color="tab:gray",
+    line2, = ax.plot(
+        bond[:, 0], bond[:, 1], color="tab:blue",
         ls="dashed", lw=4.0, zorder=0
     )
+ax.legend(
+    [line0, line1, line2], ["$t_0$", "$t_1$", "$t_1$"],
+    loc="center", fontsize=30,
+)
 
+ax.set_xlim(-4.5, 9.0)
+ax.set_ylim(-2.0, 10.0)
 ax.set_axis_off()
 ax.set_aspect("equal")
 plt.get_current_fig_manager().window.showMaximized()
